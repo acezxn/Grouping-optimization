b2a58b3a9bb3d8772e1a8eadc3d6e4c13282aa87
acezxn <daniel@Macintosh-9.local>
2020-11-13 12:50:09 +0800

    Initial commit

--- grouping_serv/settings.py
+++ grouping_serv/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

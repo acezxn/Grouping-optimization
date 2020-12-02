6fea561e6432e2a1e750cd7449447fd9c57ef3ad
acezxn <daniel@Macintosh-9.local>
2020-12-02 12:40:51 +0800

    ,

--- grouping_serv/settings.py
+++ grouping_serv/settings.py
@@ -100,10 +100,11 @@ WSGI_APPLICATION = "grouping_serv.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        "OPTIONS": {
-            "timeout": 10,
+        "ENGINE": "django.db.backends.postgresql",
+        'USER': 'postgres',
+        'NAME': 'DATABASE_URL',
+        'TEST': {
+            'NAME': 'DATABASE_URL',
         },
     }
 }

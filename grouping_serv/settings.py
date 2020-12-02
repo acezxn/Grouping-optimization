00fe1341840e00823510442e92a49f80a79926ab
acezxn <daniel@Macintosh-9.local>
2020-12-03 00:08:17 +0800

    a

--- grouping_serv/settings.py
+++ grouping_serv/settings.py
@@ -98,25 +98,25 @@ WSGI_APPLICATION = "grouping_serv.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#         "OPTIONS": {
-#             "timeout": 10,
-#         },
-#     }
-# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        'NAME': 'd1aievjol527br',
-        'HOST': 'ec2-54-235-192-146.compute-1.amazonaws.com',
-        'USER': 'vepdyhtusbtpeo',
-        'PASSWORD': 'd001c13cca42bdd136f4a08b1acaf3b778cea4aa18b73f2fc9ea6d7f47ccf101',
-        'PORT':'5432'
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "OPTIONS": {
+            "timeout": 10,
+        },
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         'NAME': 'd1aievjol527br',
+#         'HOST': 'ec2-54-235-192-146.compute-1.amazonaws.com',
+#         'USER': 'vepdyhtusbtpeo',
+#         'PASSWORD': 'd001c13cca42bdd136f4a08b1acaf3b778cea4aa18b73f2fc9ea6d7f47ccf101',
+#         'PORT':'5432'
+#     }
+# }
 
 
 # Password validation

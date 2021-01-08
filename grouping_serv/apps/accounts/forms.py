46fc46c7ad6ec388f8b74326781fcd91c162ad80
acezxn <daniel@Macintosh-9.local>
2021-01-08 23:27:49 +0800

    bug fix

--- grouping_serv/apps/accounts/forms.py
+++ grouping_serv/apps/accounts/forms.py
@@ -17,3 +17,8 @@ class NameForm(forms.Form):
     #
     #
     #     ])
+class SignupForm(forms.Form):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
